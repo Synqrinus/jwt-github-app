2257cf60268f736c1f102948d231fbffd118041d
Alex Firsov <aragorn.strider.dunadan@gmail.com>
2021-08-19 15:44:43 +0300

    Update

--- jwt-generate.sh
+++ jwt-generate.sh
@@ -12,6 +12,9 @@ thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 app_id="$APP_ID"
 app_private_key="$PRIVATE_KEY"
 lifetime="$LIFETIME"
+repo=${GITHUB_REPOSITORY:?Missing required GITHUB_REPOSITORY environment variable}
+
+[[ ! -z "$INPUT_REPO" ]] && repo=$INPUT_REPO
 
 echo "::debug::App ID $app_id"
 echo "::debug::App Secret $app_private_key"
@@ -44,18 +47,35 @@ rs256_sign() { openssl dgst -binary -sha256 -sign <(printf '%s\n' "$1"); }
 payload=$(build_payload) || return
 signed_content="$(json <<<"$header" | b64enc).$(json <<<"$payload" | b64enc)"
 sig=$(printf %s "$signed_content" | rs256_sign "$app_private_key" | b64enc)
+jwt="${signed_content}.${sig}"
 
-echo "::set-output name=token::${signed_content}.${sig}"
+check_id=$(curl -s -H "Authorization: Bearer ${signed_content}.${sig}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app | jq '.id')
 
-check_curl=$(curl -s -H "Authorization: Bearer ${signed_content}.${sig}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app)
-check_id=$(echo "$check_curl" | jq '.id')
-
-if [[ "$check_id" == "$app_id" ]]; then
-    echo "::debug::JWT Token successfully generated."
-else
-    echo "::error::Could not generate access token, expected $app_id got $check_id"
-    echo "::error::JWT ${signed_content}.${sig}"
-    echo "::debug::Curl $check_curl"
-    echo "::error::Curl $check_curl"
+if [[ ! "$check_id" == "$app_id" ]]; then
+    echo "::error::Could not generate JWT token"
     exit 1
 fi
+
+echo "::debug::JWT Token successfully generated."
+
+installation_id=$(curl -s \
+-H "Authorization: Bearer ${jwt}" \
+-H "Accept: application/vnd.github.machine-man-preview+json" \
+https://api.github.com/repos/${repo}/installation | jq -r .id)
+
+if [ "$installation_id" = "null" ]; then
+  echo "Unable to get installation ID. Is the GitHub App installed on ${repo}?"
+  exit 1
+fi
+
+token=$(curl -s -X POST \
+-H "Authorization: Bearer ${jwt}" \
+-H "Accept: application/vnd.github.machine-man-preview+json" \
+https://api.github.com/app/installations/${installation_id}/access_tokens | jq -r .token)
+
+if [ "$token" = "null" ]; then
+  echo "Unable to generate installation access token"
+  exit 1
+fi
+
+echo "::set-output name=token::${token}"
